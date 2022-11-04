c3506c4337807039ff40c47e4e282359380fee05
Yash-10 <yashgondhalekar567@gmail.com>
2022-11-04 19:52:02 +0530

    Update

--- data_utils/utils.py
+++ data_utils/utils.py
@@ -3,6 +3,9 @@ import math
 import torch.utils.data
 from collections import defaultdict
 
+import numpy as np
+from torch.utils.data import WeightedRandomSampler
+
 
 def stratified_split(dataset : torch.utils.data.Dataset, labels, fraction, random_state=None):
     if random_state: random.seed(random_state)
@@ -20,3 +23,18 @@ def stratified_split(dataset : torch.utils.data.Dataset, labels, fraction, rando
     second_set_inputs = torch.utils.data.Subset(dataset, second_set_indices)
     second_set_labels = list(map(labels.__getitem__, second_set_indices))
     return first_set_inputs, first_set_labels, second_set_inputs, second_set_labels
+
+
+def get_imbalanced_sampler(dataset):
+    dataset_indices = [d[1] for d in dataset.samples]
+    # dataset_indices = dataset.indices
+    y = [dataset.targets[i] for i in dataset_indices]
+    class_sample_count = np.array(
+        [len(np.where(y == t)[0]) for t in np.unique(y)])
+
+    weight = 1. / class_sample_count
+    samples_weight = np.array([weight[t] for t in y])
+    samples_weight = torch.from_numpy(samples_weight)
+    sampler = WeightedRandomSampler(samples_weight.type('torch.DoubleTensor'), len(samples_weight))
+
+    return sampler
